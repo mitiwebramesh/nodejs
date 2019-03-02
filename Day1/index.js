82d1c8a0a59b6cded7efa6e3c8e7bc5be68be67c
Ramesh <ramesh@aroha.co.in>
2019-03-02 12:59:35 +0530

    read file

--- Day1/index.js
+++ Day1/index.js
@@ -1,16 +1,23 @@
 let http = require('http');
 let url = require('url');
+let fs = require('fs');
 
 http.createServer(function (request, response) {
     const path = url.parse(request.url).pathname;
     if (path == '/') {
-        response.write('This is Root')
+        response.write('This is Root');
+        response.end();
     } else if (path == '/home') {
+        fs.readFile('sample.txt', (err, data) => {
+            response.write(data.toString());
+            response.end();
+        })
         response.write('This is Home')
     } else if (path == '/aboutus') {
         response.write('This is about us');
+        response.end();
     }
     console.log('Get Some Request from ', path);
     // response.write('Hello World');
-    response.end();
+
 }).listen(5000);
